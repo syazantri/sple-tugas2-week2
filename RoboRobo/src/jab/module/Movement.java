d7bc53e067a06a20b2d91551c3c9085b762f42ce
syazantri <syazantri@gmail.com>
2025-03-14 23:25:08 +0700

    feat: fitur 4 part 1

--- RoboRobo/src/jab/module/Movement.java
+++ RoboRobo/src/jab/module/Movement.java
@@ -18,39 +18,13 @@ public class Movement extends Part {
 	}
 
 	public void move() {
-		// Limit our speed
-		bot.setMaxVelocity(6);
-
-		// Continue in the current move direction
-		bot.setAhead(moveDirection * 20);
-
-		// Track distance traveled
-		distanceTraveled += Math.abs((int) bot.getVelocity());
-
-		// If we've gone far enough, change direction
-		if (distanceTraveled >= zigzagDistance) {
-			turnDirection *= -1;
-			bot.setTurnRight(45 * turnDirection);
-			distanceTraveled = 0;
-		}
+		bot.setAhead(0.008);
 	}
 
 	public void listenInput(InputEvent e) {
 	}
 
 	public void listen(Event e) {
-		if (e instanceof robocode.HitWallEvent) {
-			// Reverse direction and turn away from wall
-			moveDirection *= -1;
-			turnDirection *= -1;
-			bot.setTurnRight(90 * turnDirection);
-			distanceTraveled = 0;
-		}
 	}
-
-	int moveDirection = 1;
-	int turnDirection = 1;
-	int zigzagDistance = 2;
-	int distanceTraveled = 0;
 
 }
--- RoboRobo/src/jab/module/SelectEnemy.java
+++ RoboRobo/src/jab/module/SelectEnemy.java
@@ -1,25 +1,30 @@
-package jab.module; 
+package jab.module;
 
 /**
  * Select enemy
  * 
  * @author jabier.martinez
  */
-public class  SelectEnemy  extends Part {
-	
+public class SelectEnemy extends Part {
 
 	public Module bot;
-
-	
 
 	public SelectEnemy(Module bot) {
 		this.bot = bot;
 	}
 
-	
-
 	public void select() {
+		if (bot.botsInfo.isEmpty()) {
+			bot.enemy = null;
+			return;
+		}
+
+		Object[] enemies = bot.botsInfo.values().toArray();
+
+		int randomIndex = random.nextInt(enemies.length);
+		bot.enemy = (BotInfo) enemies[randomIndex];
 	}
 
+	java.util.Random random = new java.util.Random();
 
 }
